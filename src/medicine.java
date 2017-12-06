378470ebcb1973dd4856d6f2f7108ceb65024341
jasmiinet <jxt1672@louisiana.edu>
2017-12-05 20:30:19 -0600

    Updated Final Changes.: :

--- src/medicine.java
+++ src/medicine.java
@@ -14,14 +14,16 @@ public class medicine {
         System.out.println("do you want to enter time of dose?");
         char n = s.next().charAt(0);
         int time =0;
-        int amount;
+        int amount=0;
         SimpleDateFormat time2= new SimpleDateFormat("HH:mm:ss");
         Date date = new Date();
         if (n=='y'){
-                    System.out.println("Enter time of dosage");
-                    String time_new = s.nextLine();
-                    time = ordinalDate(,,);
+                    System.out.println("Enter amount  of medicine in (mg):");
+                    amount = s.nextInt();
         }
+        System.out.println("Enter time of dosage:");
+        time = s.nextInt();
+        int half = time/2;
         System.out.println("The medicine details are ");
         System.out.println("Medicine :"+medicine);
         System.out.println("Time of dosage:");
