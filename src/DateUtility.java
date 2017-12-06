c63c8ffd7f1e35b0aaeaabcf8be260eb61eab9fb
jasmiinet <jxt1672@louisiana.edu>
2017-12-05 19:24:10 -0600

    Added days in method under Date Utility

--- src/DateUtility.java
+++ src/DateUtility.java
@@ -6,7 +6,23 @@ public class DateUtility {
     
     @SuppressWarnings("empty-statement")
     public static int daysIn(int month,int year){
-       return 0;
+       
+        if (year <1 )
+             throw new IllegalArgumentException ("daysIn : year ("+year+") not positive");
+        if (month< JANUARY || month > DECEMBER)             
+            throw new IllegalArgumentException ("daysIn : year ("+year+") not in range [1,12]");
+        if ( month == APRIL || month ==JUNE || month == SEPTEMBER || month == NOVEMBER )
+               return 30;
+        else if (month == JANUARY || month == MARCH || month == MAY || month == JULY || month == AUGUST || month == OCTOBER || month == DECEMBER)
+                return 31;
+        else 
+               return 28 + (isLeapYear(year)?1:0);
+    }
+    
+    public static ordinalDate(int month, int day, int year){
+        
     }
 
 }
+
+
